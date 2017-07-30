b93683b9835c84a5ce82b35020413d5d5422761c
KeremAtak <kerem.atak@hotmail.com>
2017-07-30 07:00:15 +0300

    v1

---
+++ labraharava/src/test/java/labraharava/test/test.java
@@ -0,0 +1,16 @@
+package labraharava.test;
+
+
+
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class test {
+    
+    
+    @Test
+    public void test() {
+        assertTrue(1 == 1);
+    }
+}
